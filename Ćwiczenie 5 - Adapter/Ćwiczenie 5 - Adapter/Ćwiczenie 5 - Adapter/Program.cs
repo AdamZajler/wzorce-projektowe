ec19d2b09026d1e13f4396a9d0721390b88d2c6f
Adam Zajler <adamzajler01@gmail.com>
2024-03-24 10:20:13 +0100

    Added ex. 5

---
+++ Ćwiczenie 5 - Adapter/Ćwiczenie 5 - Adapter/Ćwiczenie 5 - Adapter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+interface IOldLogger
+{
+    void LogMessage(string message);
+}
+
+interface INewLogger
+{
+    void LogMessage(string message);
+}
+public class NewLogger : INewLogger
+{
+    public void LogMessage(string message)
+    {
+        Console.WriteLine("{0}", message);
+    }
+}
+
+class LoggerAdapter : IOldLogger
+{
+    private readonly INewLogger _newLogger;
+
+    public LoggerAdapter(INewLogger newLogger)
+    {
+        _newLogger = newLogger;
+    }
+
+    public void LogMessage(string message)
+    {
+        _newLogger.LogMessage(message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        INewLogger newLogger = new NewLogger();
+
+        IOldLogger adapter = new LoggerAdapter(newLogger);
+
+        adapter.LogMessage("Komunikat 1");
+        adapter.LogMessage("Komunikat 2");
+    }
+}
