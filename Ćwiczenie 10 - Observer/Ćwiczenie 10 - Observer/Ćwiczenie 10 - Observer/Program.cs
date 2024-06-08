be7f727bed80122f384713c49414a16965145b9c
Adam Zajler <adamzajler01@gmail.com>
2024-06-08 14:52:25 +0200

    Added ex. 9 & 10

---
+++ Ćwiczenie 10 - Observer/Ćwiczenie 10 - Observer/Ćwiczenie 10 - Observer/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IObserver
+{
+    void UpdateStatus(Dictionary<string, int> scores);
+}
+
+public interface ISubject
+{
+    void AddObserver(IObserver observer);
+    void RemoveObserver(IObserver observer);
+    void NotifyObservers();
+}
+
+public class Player : IObserver
+{
+    private string playerName;
+    private ISubject gameNotifier;
+
+    public Player(string playerName, ISubject gameNotifier)
+    {
+        this.playerName = playerName;
+        this.gameNotifier = gameNotifier;
+        gameNotifier.AddObserver(this);
+    }
+
+    public void UpdateStatus(Dictionary<string, int> scores)
+    {
+        Console.WriteLine($"{playerName}'s updated scores:");
+        foreach (var score in scores)
+        {
+            Console.WriteLine($"Player: {score.Key}, Score: {score.Value}");
+        }
+        Console.WriteLine();
+    }
+}
+
+public class Game : ISubject
+{
+    private List<IObserver> observers;
+    private Dictionary<string, int> playerScores;
+
+    public Game()
+    {
+        observers = new List<IObserver>();
+        playerScores = new Dictionary<string, int>();
+    }
+
+    public void AddObserver(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void RemoveObserver(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    public void NotifyObservers()
+    {
+        foreach (var observer in observers)
+        {
+            observer.UpdateStatus(playerScores);
+        }
+    }
+
+    public void SetScore(string playerName, int score)
+    {
+        if (playerScores.ContainsKey(playerName))
+        {
+            playerScores[playerName] = score;
+        }
+        else
+        {
+            playerScores.Add(playerName, score);
+        }
+        NotifyObservers();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Game game = new Game();
+
+        Player player1 = new Player("Adam", game);
+        Player player2 = new Player("John", game);
+        Player player3 = new Player("Michael", game);
+
+        game.SetScore("Adam", 10);
+        game.SetScore("John", 15);
+        game.SetScore("Michael", 8);
+        game.SetScore("Adam", 20);
+
+        Console.ReadKey();
+    }
+}
---
+++ Ćwiczenie 9 - Observer/Ćwiczenie 9 - Observer/Ćwiczenie 9 - Observer/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadanie_9
+{
+    interface IListener
+    {
+        void OnUpdate(MeteorologicalData data);
+    }
+
+    interface INotifier
+    {
+        void Register(IListener listener);
+        void Unregister(IListener listener);
+        void NotifyListeners(MeteorologicalData data);
+    }
+
+    class MeteorologicalData
+    {
+        public double Temperature;
+        public string Cloudiness;
+        public int PrecipitationProbability;
+
+        public MeteorologicalData(double temperature, string cloudiness, int precipitationProbability)
+        {
+            this.Temperature = temperature;
+            this.Cloudiness = cloudiness;
+            this.PrecipitationProbability = precipitationProbability;
+        }
+    }
+
+    class ConcreteListener : IListener
+    {
+        private string listenerName;
+        public string[] Parameters;
+
+        public ConcreteListener(string listenerName, string[] parameters)
+        {
+            this.listenerName = listenerName;
+            this.Parameters = parameters;
+        }
+
+        public void OnUpdate(MeteorologicalData data)
+        {
+            foreach (string parameter in Parameters)
+            {
+                var singleData = typeof(MeteorologicalData).GetField(parameter).GetValue(data);
+
+                if (singleData != null)
+                {
+                    Console.WriteLine(listenerName + ": " + singleData);
+                }
+            }
+        }
+    }
+
+    class WeatherMonitoringStation : INotifier
+    {
+        private List<IListener> listeners = new List<IListener>();
+        private MeteorologicalData data;
+
+        public void Register(IListener listener)
+        {
+            listeners.Add(listener);
+        }
+
+        public void Unregister(IListener listener)
+        {
+            listeners.Remove(listener);
+        }
+
+        public void NotifyListeners(MeteorologicalData data)
+        {
+            foreach (IListener listener in listeners)
+            {
+                listener.OnUpdate(data);
+            }
+        }
+
+        public void UpdateData(MeteorologicalData data)
+        {
+            this.data = data;
+            NotifyListeners(this.data);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherMonitoringStation weatherStation = new WeatherMonitoringStation();
+
+            ConcreteListener tempListener = new ConcreteListener("Temperature Listener", new string[] { "Temperature" });
+            ConcreteListener cloudinessListener = new ConcreteListener("Cloudiness Listener", new string[] { "Cloudiness" });
+            ConcreteListener rainChanceListener = new ConcreteListener("Rain Chance Listener", new string[] { "PrecipitationProbability" });
+
+            weatherStation.Register(tempListener);
+            weatherStation.Register(cloudinessListener);
+            weatherStation.Register(rainChanceListener);
+
+            weatherStation.UpdateData(new MeteorologicalData(25.00, "Cloudy", 70));
+
+            Console.ReadKey();
+        }
+    }
+}
